307f01660ab804002527d4ecd5f4af8f1bbca67f
Marishka95s <marishka95@ukr.net>
2021-09-08 16:24:41 +0300

    Mistake in name :(

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -4,7 +4,7 @@ import image from '../images/phonebook.png';
 export default function HomeView() {    
     return(
     <section className={styles.home}>
-        <h2 className={styles.heading}>Welcome to your most convenient phonebok :)</h2>
+        <h2 className={styles.heading}>Welcome to your most convenient phonebook :)</h2>
         <img src={image} alt="phonebook" width="300"/>
     </section>)
 }
